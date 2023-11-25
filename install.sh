260d946eec522a7ca013768c11f5fcbfe6f9a25c
Drew Teeter <drew@drewteeter.com>
2023-11-25 14:40:24 -0600

    Running font install with sudo, to install for all users

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ ifonts() {
     wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
     wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
     wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-    cp *.ttf /usr/share/fonts
-    fc-cache -f -v
+    sudo cp *.ttf /usr/share/fonts
+    sudo fc-cache -f -v
     rm *.tff
 }
 
